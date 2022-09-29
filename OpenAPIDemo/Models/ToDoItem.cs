97889e16227ad03d822a35c875de4164970363d7
Brett Hargreaves <bretthargreaves@hotmail.com>
2022-09-29 10:02:11 +0100

    added linkrul to todo items

--- OpenAPIDemo/Models/ToDoItem.cs
+++ OpenAPIDemo/Models/ToDoItem.cs
@@ -6,6 +6,14 @@ namespace OpenAPIDemo.Models
         public string Description { get; set; }
         public bool IsCompleted { get; set; }
 
+        public string LinkUrl
+        {
+            get { 
+                    return "https://openapidemo.azurewebsites.net/api/todoitems/" + Id;
+            }
+            private set{}
+        }
+
         public ToDoItem()
         {
             this.Id = Guid.NewGuid().ToString();
